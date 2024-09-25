848265673d2818f4f8acaa1fa4e3d862ac09ba4e
Sanjulaarora <sanjulaarora3005@gmail.com>
2024-09-25 16:04:32 +0530

    home component further created

--- src/components/home/MainComp.js
+++ src/components/home/MainComp.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner';
 import './home.css';
+import Slide from './Slide';
 
 const MainComp = () => {
   return (
@@ -8,6 +9,22 @@ const MainComp = () => {
       <div className="banner_part">
         <Banner />
       </div>
+      <div className="slide_part">
+        <div className="left_slide">
+          <Slide title="Deal of the Day" />
+        </div>
+        <div className="right_slide">
+          <h4>Festive Latest Launches</h4>
+          <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/Jupiter/Launches/T3/DesktopGateway_CategoryCard2x_758X608_T3._SY608_CB639883570_.jpg" alt="right_image" />
+          <a href="#">See More!</a>
+        </div>
+      </div>
+      <Slide title="Today's Deal" />
+      <div className="center_img">
+        <img src="https://m.media-amazon.com/images/G/31/AMS/IN/970X250-_desktop_banner.jpg" alt="center_img"/>
+      </div>
+      <Slide title="Best Seller" />
+      <Slide title="Upto 80% off" />
     </div>
   )
 }
