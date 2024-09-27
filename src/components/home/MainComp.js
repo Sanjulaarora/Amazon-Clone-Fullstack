a79a381dc134ed1a96dae0eadf3389523d81ca6d
Sanjulaarora <sanjulaarora3005@gmail.com>
2024-09-27 17:15:34 +0530

    redux used for state management

--- src/components/home/MainComp.js
+++ src/components/home/MainComp.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Banner from './Banner';
 import './home.css';
 import Slide from './Slide';
+import { getProducts } from '../redux/actions/action';
+import { useDispatch, useSelector } from 'react-redux';
 
 const MainComp = () => {
+  const { products } = useSelector(state => state.getproductsdata);
+  console.log(products);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
   return (
     <div className="home_section">
-      <div className="banner_part">
+      <div className="banner_part"> 
         <Banner />
       </div>
       <div className="slide_part">
         <div className="left_slide">
-          <Slide title="Deal of the Day" />
+          <Slide title="Deal of the Day" products={products}/>
         </div>
         <div className="right_slide">
           <h4>Festive Latest Launches</h4>
@@ -19,12 +30,12 @@ const MainComp = () => {
           <a href="#">See More!</a>
         </div>
       </div>
-      <Slide title="Today's Deal" />
+      <Slide title="Today's Deal" products={products} />
       <div className="center_img">
         <img src="https://m.media-amazon.com/images/G/31/AMS/IN/970X250-_desktop_banner.jpg" alt="center_img"/>
       </div>
-      <Slide title="Best Seller" />
-      <Slide title="Upto 80% off" />
+      <Slide title="Best Seller" products={products} />
+      <Slide title="Upto 80% off" products={products} />
     </div>
   )
 }
---
+++ src/components/redux/reducers/main.js
@@ -0,0 +1,9 @@
+import { getProductsreducer } from "./ProductsReducers";
+import { combineReducers } from "redux";
+
+const rootreducers = combineReducers({
+    getproductsdata:getProductsreducer
+});
+
+
+export default rootreducers;
